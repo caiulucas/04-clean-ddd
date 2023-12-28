acf8a8630e2834911393899c7ec5fe9af1e1fbf3
Caio Lucas <lienscarlet@gmail.com>
2023-12-28 17:44:18 -0300

    feat: add slug class

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,14 +1,16 @@
 import { randomUUID } from 'crypto';
-
+import { Slug } from './value-objects/slug';
 interface QuestionProps {
 	title: string;
 	content: string;
+	slug: Slug;
 	authorId: string;
 }
 export class Question {
 	public id: string;
 	public authorId: string;
 	public title: string;
+	public slug: Slug;
 	public content: string;
 
 	constructor(props: QuestionProps, id?: string) {
@@ -16,5 +18,6 @@ export class Question {
 		this.authorId = props.authorId;
 		this.title = props.title;
 		this.content = props.content;
+		this.slug = props.slug;
 	}
 }
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+	public value: string;
+
+	constructor(value: string) {
+		this.value = value;
+	}
+
+	/**
+	 * Receives a text string and normalize it as a slug.
+	 *
+	 * @param text {string}
+	 *
+	 * @example "An example title" => "an-example-title"
+	 */
+	static createFromText(text: string) {
+		const slugText = text
+			.normalize('NFKC')
+			.toLowerCase()
+			.trim()
+			.replace(/\s+/g, '-')
+			.replace(/[^\w-]+/g, '')
+			.replace(/_/g, '-')
+			.replace(/--+/g, '-')
+			.replace(/-$/, '');
+
+		return new Slug(slugText);
+	}
+}
