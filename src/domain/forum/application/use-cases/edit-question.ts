d01c1b945cecaf8f54e53f061d012aa5072a685b
Caio Lucas <lienscarlet@gmail.com>
2023-12-29 19:19:27 -0300

    feat: edit author

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,41 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { Question } from '../../enterprise/entities/question';
+import { QuestionsRepository } from '../repositories/questions-repository';
+
+interface EditQuestionUseCaseRequest {
+	authorId: string;
+	questionId: string;
+	title: string;
+	content: string;
+}
+
+interface EditQuestionUseCaseResponse {
+	question: Question;
+}
+
+export class EditQuestionUseCase {
+	constructor(private questionsRepository: QuestionsRepository) {}
+
+	async execute(
+		request: EditQuestionUseCaseRequest,
+	): Promise<EditQuestionUseCaseResponse> {
+		const question = await this.questionsRepository.findById(
+			request.questionId,
+		);
+
+		if (!question) {
+			throw new Error('Resource Not found.');
+		}
+
+		if (request.authorId !== question.authorId.toValue()) {
+			throw new Error('Not allowed.');
+		}
+
+		question.title = request.title;
+		question.content = request.content;
+
+		await this.questionsRepository.save(question);
+
+		return { question };
+	}
+}
---
+++ tests/domain/forum/application/use-cases/edit-question.test.ts
@@ -0,0 +1,49 @@
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question';
+import { makeQuestion } from '../factories/make-question';
+import { InMemoryQuestionsRepository } from '../repositories/in-memory-questions-repository';
+
+describe('Edit Question Use Case', () => {
+	let questionsRepository: InMemoryQuestionsRepository;
+	let sut: EditQuestionUseCase;
+
+	beforeEach(() => {
+		questionsRepository = new InMemoryQuestionsRepository();
+		sut = new EditQuestionUseCase(questionsRepository);
+	});
+
+	it('should be able to edit a question', async () => {
+		const newQuestion = makeQuestion();
+
+		await questionsRepository.create(newQuestion);
+
+		const editedQuestion = {
+			authorId: newQuestion.authorId.toValue(),
+			questionId: newQuestion.id.toValue(),
+			title: 'Edited question',
+			content: 'This is a new content',
+		};
+
+		await sut.execute(editedQuestion);
+
+		expect(questionsRepository.items[0]).toMatchObject({
+			title: editedQuestion.title,
+			content: editedQuestion.content,
+		});
+	});
+
+	it('should be not able to edit a question from another author', async () => {
+		const newQuestion = makeQuestion();
+
+		await questionsRepository.create(newQuestion);
+
+		await expect(
+			async () =>
+				await sut.execute({
+					questionId: newQuestion.id.toValue(),
+					authorId: 'some-author-id',
+					title: 'Edited question',
+					content: 'This is a new content',
+				}),
+		).rejects.toThrowError();
+	});
+});
